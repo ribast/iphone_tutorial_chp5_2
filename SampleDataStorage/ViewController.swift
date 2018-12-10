dc178822d5538070701c144eb6eaf89355e18e10
田島諒 <ryo_tajima@tajimaryounomac-mini.local>
2018-12-10 23:06:47 +0900

    Initial Commit

---
+++ SampleDataStorage/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleDataStorage
+//
+//  Created by 田島諒 on 2018/12/10.
+//  Copyright © 2018 Ribast. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
